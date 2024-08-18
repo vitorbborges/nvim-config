0ac886c597764406df9ee706c016f1622fd3e355
vitorbborges <vitorbborges31@gmail.com>
2024-08-18 17:33:03 -0300

    lsp changes

--- lua/vitor/plugins/formatting.lua
+++ lua/vitor/plugins/formatting.lua
@@ -1,89 +1,87 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local conform = require("conform")
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
 
-    conform.setup({
-      formatters_by_ft = {
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        lua = { "stylua" },
-        python = { "ruff_organize_imports", "ruff_format" },
-        bibtex = { "bibtex-tidy" },
-        latex = { "latexindent" },
-        sql = { "sql-formatter" },
-        rust = { "ast_grep" },
-        c = { "ast_grep" },
-        cpp = { "ast_grep" },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
-    })
+        conform.setup({
+            formatters_by_ft = {
+                json = { "prettier" },
+                yaml = { "prettier" },
+                markdown = { "prettier" },
+                lua = { "stylua" },
+                python = { "ruff_organize_imports", "ruff_format", "isort", "black" },
+                bibtex = { "bibtex-tidy" },
+                latex = { "latexindent" },
+                sql = { "sql-formatter" },
+                rust = { "rustfmt" },
+                c = { "clang-format" },
+                cpp = { "clang-format" },
+            },
+            format_on_save = {
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 1000,
+            },
+        })
 
-    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-      conform.format({
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      })
-    end, { desc = "Format file or range (in visual mode)" })
+        vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+            conform.format({
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 1000,
+            })
+        end, { desc = "Format file or range (in visual mode)" })
 
-    conform.setup({
-      format_on_save = function(bufnr)
-        -- Disable with a global or buffer-local variable
-        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-          return
-        end
-        return { timeout_ms = 500, lsp_format = "fallback" }
-      end,
-    })
+        conform.setup({
+            format_on_save = function(bufnr)
+                -- Disable with a global or buffer-local variable
+                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                    return
+                end
+                return { timeout_ms = 500, lsp_format = "fallback" }
+            end,
+        })
 
-    vim.api.nvim_create_user_command("FormatDisable", function(args)
-      if args.bang then
-        -- FormatDisable! will disable formatting just for this buffer
-        vim.b.disable_autoformat = true
-      else
-        vim.g.disable_autoformat = true
-      end
-    end, {
-      desc = "Disable autoformat-on-save",
-      bang = true,
-    })
-    vim.api.nvim_create_user_command("FormatEnable", function()
-      vim.b.disable_autoformat = false
-      vim.g.disable_autoformat = false
-    end, {
-      desc = "Re-enable autoformat-on-save",
-    })
+        vim.api.nvim_create_user_command("FormatDisable", function(args)
+            if args.bang then
+                -- FormatDisable! will disable formatting just for this buffer
+                vim.b.disable_autoformat = true
+            else
+                vim.g.disable_autoformat = true
+            end
+        end, {
+            desc = "Disable autoformat-on-save",
+            bang = true,
+        })
+        vim.api.nvim_create_user_command("FormatEnable", function()
+            vim.b.disable_autoformat = false
+            vim.g.disable_autoformat = false
+        end, {
+            desc = "Re-enable autoformat-on-save",
+        })
 
-    local slow_format_filetypes = {}
-    conform.setup({
-      format_on_save = function(bufnr)
-        if slow_format_filetypes[vim.bo[bufnr].filetype] then
-          return
-        end
-        local function on_format(err)
-          if err and err:match("timeout$") then
-            slow_format_filetypes[vim.bo[bufnr].filetype] = true
-          end
-        end
+        local slow_format_filetypes = {}
+        conform.setup({
+            format_on_save = function(bufnr)
+                if slow_format_filetypes[vim.bo[bufnr].filetype] then
+                    return
+                end
+                local function on_format(err)
+                    if err and err:match("timeout$") then
+                        slow_format_filetypes[vim.bo[bufnr].filetype] = true
+                    end
+                end
 
-        return { timeout_ms = 200, lsp_format = "fallback" }, on_format
-      end,
+                return { timeout_ms = 200, lsp_format = "fallback" }, on_format
+            end,
 
-      format_after_save = function(bufnr)
-        if not slow_format_filetypes[vim.bo[bufnr].filetype] then
-          return
-        end
-        return { lsp_format = "fallback" }
-      end,
-    })
-  end,
+            format_after_save = function(bufnr)
+                if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+                    return
+                end
+                return { lsp_format = "fallback" }
+            end,
+        })
+    end,
 }
