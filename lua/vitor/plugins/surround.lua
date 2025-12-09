b0eb3df7fdd1f9f2a8b7b7d0996c055fbd4adbfe
vitorbborges <vitorbborges31@gmail.com>
2025-12-09 12:10:09 -0300

    adding descriptions for the sorround plugin

--- lua/vitor/plugins/surround.lua
+++ lua/vitor/plugins/surround.lua
@@ -2,6 +2,15 @@ return {
     "kylechui/nvim-surround",
     event = { "BufReadPre", "BufNewFile" },
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    config = true,
-    -- TODO: Understand this plugin
+    config = function()
+        require("nvim-surround").setup()
+
+        local wk = require("which-key")
+        wk.add({
+            { "ys", desc = "ys{motion}{char} - Add surrounding (e.g., ysiw)", mode = "n" },
+            { "ds", desc = "ds{char} - Delete surrounding (e.g., ds')", mode = "n" },
+            { "cs", desc = "cs{target}{replacement} - Change surrounding (e.g., cs'\" to change ' to \")", mode = "n" },
+            { "S", desc = 'S{char} - Surround visual selection (e.g., S" for "selection")', mode = "v" },
+        })
+    end,
 }
