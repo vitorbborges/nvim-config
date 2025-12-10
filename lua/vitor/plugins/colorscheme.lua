cb52d5868eae80688f0fa37e21a3b67a8901ee24
vitorbborges <vitorbborges31@gmail.com>
2025-12-10 09:19:44 -0300

    Improving documentation of carpuccin

--- lua/vitor/plugins/colorscheme.lua
+++ lua/vitor/plugins/colorscheme.lua
@@ -1,5 +1,10 @@
+-- catppuccin/nvim: A soothing pastel theme for Neovim.
+-- This plugin provides the Catppuccin color scheme, which is designed to be
+-- easy on the eyes while maintaining clarity. It comes in four "flavors":
+-- Latte, Frappé, Macchiato, and Mocha. The current setup uses the "Mocha" flavor.
+-- The plugin offers deep integration with many other plugins for a cohesive look.
+-- For more information, see: https://github.com/catppuccin/nvim
 return {
-    -- TODO: Understand/Improve this.
     "catppuccin/nvim",
     as = "catppuccin",
     priority = 1000,
